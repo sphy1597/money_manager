c1e737a70f031107b3896459399e63af36966118
sphy1597 <sphy1597@naver.com>
2023-11-23 18:49:36 +0900

    feat: 사용자 조건에 따라 지출 검색 기능 추가

--- server/controller/spending.js
+++ server/controller/spending.js
@@ -1,7 +1,9 @@
 const jwt = require("../utils/jwt_utils");
 const { User, Spending, Budget } = require("../models");
+const { Op } = require("sequelize");
 
 // 지출 설정
+// note: 동일한 카테고리가 있을 경우 제외,
 const postSetSpending = async (req, res) => {
   const { user_id, category, price, comment } = req.body;
   const spending = await Spending.create({
@@ -51,18 +53,76 @@ const deleteSpending = async (req, res) => {
 };
 
 // 지출 확인
-// note :  카테고리별, 기간별 조회 기능 구현
 const getSpending = async (req, res) => {
-  const { spending_id, user_id } = req.body;
+  const { user_id, category, startDate, endDate, minAmount, maxAmount } =
+    req.body;
 
-  const result = await Spending.findAll({
-    attributes: ["id", "user_id", "category", "price", "comment"],
-    where: { id: spending_id, user_id: user_id },
-  });
-  console.log("result : ", result);
+  const userFilters = {
+    user_id,
+    category,
+    startDate,
+    endDate,
+    minAmount,
+    maxAmount,
+  };
+  const result = await searchSpending(userFilters);
+  console.log("조회 결과 : ", result);
   res.json({
     result: result,
   });
+};
+
+// 사용자의 조건에 따라 지출 검색
+const searchSpending = async (userFilters) => {
+  // Sequelize의 where 객체를 동적으로 구성
+
+  // 필터링할 조건을 저장할 빈 객체
+  const whereClause = {
+    user_id: userFilters.user_id, // user_id 필터 추가
+  };
+
+  // 카테고리
+  userFilters.category && (whereClause.category = userFilters.category);
+
+  // createdAt에 대한 필터링을 위한 객체
+  const dateClause = {};
+
+  // startDate
+  dateClause[Op.gte] = userFilters.startDate || undefined;
+
+  // endDate
+  dateClause[Op.lte] = userFilters.endDate || undefined;
+
+  if (
+    userFilters.startDate === undefined &&
+    userFilters.endDate === undefined
+  ) {
+    userFilters.startDate = new Date();
+    userFilters.endDate = new Date();
+  }
+
+  // dateClause가 비어있지 않은 경우 createdAt 필터 추가
+  Object.keys(dateClause).length > 0 && (whereClause.createdAt = dateClause);
+
+  // amount에 대한 필터링을 위한 객체
+  const amountClause = {};
+
+  // minAmount 필터 추가 (만약 주어졌을 경우에만)
+  amountClause[Op.gte] = userFilters.minAmount || undefined;
+
+  // maxAmount 필터 추가 (만약 주어졌을 경우에만)
+  amountClause[Op.lte] = userFilters.maxAmount || undefined;
+
+  // amountClause가 비어있지 않은 경우 amount 필터 추가
+  Object.keys(amountClause).length > 0 && (whereClause.amount = amountClause);
+
+  // Spending 테이블에서 조건에 맞는 데이터 검색
+  const result = await Spending.findAll({
+    where: whereClause,
+  }).catch((err) => {
+    console.error("조건에 맞는 지출 데이터를 가져오는 중 오류 발생:", err);
+  });
+  return result;
 };
 
 module.exports = {
